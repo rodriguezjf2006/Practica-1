632945b7f9bfa840fecd1a07ce693d535450ceca
rodriguezjf2006 <rodriguezjf2006@gmail.com>
2026-03-25 19:09:31 -0300

    question.py
    4) Yo tome que las rondas repetidas son que el juego no para y por eso puse una categoria llamada fin para dejar de jugar.
    Para que no aparezcan palabras repetidas decidi usar el .remove porque me parecia mas eficaz que el random.sample, aunque abajo del .remove como comentario puse una posible solucion usando el random.sample.
    Ademas el programa frenara si ya no quedan palabras para elegir

--- question.py
+++ question.py
@@ -23,63 +23,93 @@ words = {"programacion":[
                          "informatica",
                          "medicina",
                          ]
+         , "fin":1
 }
 
+cant=len(words)-1
 #Le pido que elija la categoria
-cat=input(f"Elija una categoria : {" - ".join(words)} : ")
+cat=input(f"Elija una categoria : {" - ".join(words)} (fin para terminar el juego) : ")
 
 while not(cat in words):
-    cat=input(f"Elija una categoria dentro de las opciones: {" - ".join(words)} : ")
+    cat=input(f"Elija una categoria dentro de las opciones: {" - ".join(words)} (fin para terminar el juego): ")
 
-word = random.choice(words[cat])
-guessed = []
-attempts = 6
-puntaje = 0
+while cat!="fin" and cant > 0
+    #Si la categoria elegida tiene palabras disponibles elige una de ellas
+    if len(words[cat]) > 0
+        word = random.choice(words[cat])
+        guessed = []
+        attempts = 6
+        puntaje = 0
 
-print("¡Bienvenido al Ahorcado!")
-print()
+        print("¡Bienvenido al Ahorcado!")
+        print()
 
-while attempts > 0:
-    # Mostrar progreso: letras adivinadas y guiones para las que faltan
-    progress = ""
-    for letter in word:
-        if letter in guessed:
-            progress += letter + " "
-        else:
-            progress += "_ "
-    print(progress)
+        while attempts > 0:
+            # Mostrar progreso: letras adivinadas y guiones para las que faltan
+            progress = ""
+            for letter in word:
+                if letter in guessed:
+                    progress += letter + " "
+                else:
+                    progress += "_ "
+            print(progress)
 
-    # Verificar si el jugador ya adivinó la palabra completa
-    if "_" not in progress:
-        puntaje += 6
-        print("¡Ganaste!")
-        print(f"Puntaje: {puntaje}")
-        break
+            # Verificar si el jugador ya adivinó la palabra completa
+            if "_" not in progress:
+                puntaje += 6
+                print("¡Ganaste!")
+                print(f"Puntaje: {puntaje}")
+                words[cat].remove(word)
+              
+                #lista=random.sample(words[cat],len(words[cat])-1)
+                #while word in lista:
+                  #lista=random.sample(words[cat],len(words[cat])-1)
+                #words[cat]=lista
+              
+                break
   
-    print(f"Intentos restantes: {attempts}")
-    print(f"Letras usadas: {', '.join(guessed)}")
+            print(f"Intentos restantes: {attempts}")
+            print(f"Letras usadas: {', '.join(guessed)}")
 
-    # Si el caracter ingresado es mayuscula la paso a minuscula
-    letter = (input("Ingresá una letra: ")).lower
+            # Si el caracter ingresado es mayuscula la paso a minuscula
+            letter = (input("Ingresá una letra: ")).lower
 
-    # Verifico si el caracter es valido o no
-    if (letter in string.ascii_lowercase) and (letter!=""):
-        if letter in guessed:
-            print("Ya usaste esa letra.")
-        elif letter in word:
-            guessed.append(letter)
-            print("¡Bien! Esa letra está en la palabra.")
-        else:
-            guessed.append(letter)
-            attempts -= 1
-            puntaje -= 1
-            print("Esa letra no está en la palabra.")
-    else:
-        print("entrada no valida")
+            # Verifico si el caracter es valido o no
+            if (letter in string.ascii_lowercase) and (letter!=""):
+                if letter in guessed:
+                    print("Ya usaste esa letra.")
+                elif letter in word:
+                    guessed.append(letter)
+                    print("¡Bien! Esa letra está en la palabra.")
+                else:
+                    guessed.append(letter)
+                    attempts -= 1
+                    puntaje -= 1
+                    print("Esa letra no está en la palabra.")
+            else:
+                print("Entrada no valida")
         
-    print()
+            print()
 
-else:
-    puntaje = 0
-    print(f"¡Perdiste! La palabra era: {word}")
-    print(f"Puntaje: {puntaje}")
+        else:
+            puntaje = 0
+            print(f"¡Perdiste! La palabra era: {word}")
+            print(f"Puntaje: {puntaje}")
+          
+        cat=input(f"Elija una categoria : {" - ". join(words)} (fin para terminar el juego) : ")
+        while not(cat in words):
+            cat=input(f"Elija una categoria dentro de las opciones : {" - ".join(words)} (fin para terminar el juego) : ")
+
+    #Si la categoria ingresada ya no posee mas palabras, la saco de las opciones posibles
+    else:
+        print("Esa categoria ya no posee mas palabras")
+        words.pop(cat)
+        cant-=1
+        if cant==0:
+            break
+          
+        cat=input(f"Elija una categoria : {" - ". join(words)} (fin para terminar el juego) : ")
+        while not(cat in words):
+            cat=input(f"Elija una categoria dentro de las opciones : {" - ". join(words)} (fin para terminar el juego) : ")
+
+print("El juego termino")
